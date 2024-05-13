17cf70c4b631db3dcffcc69eaef7ce7cc0dea648
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-05-13 18:56:33 +0200

    vermeer: Initial skeleton device tree
    Change-Id: I9e0bf9d4c77dae48b97a78ac74912e034eea49d9

---
+++ device.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2024 Paranoid Android
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from sm8550-common
+$(call inherit-product, device/xiaomi/sm8550-common/common.mk)
+
+# Get non-open-source specific aspects
+$(call inherit-product, vendor/xiaomi/vermeer/vermeer-vendor.mk)
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Properties
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/properties/odm_CN.prop:$(TARGET_COPY_OUT_ODM)/etc/build_CN.prop \
+    $(LOCAL_PATH)/configs/properties/odm_GL.prop:$(TARGET_COPY_OUT_ODM)/etc/build_GL.prop
