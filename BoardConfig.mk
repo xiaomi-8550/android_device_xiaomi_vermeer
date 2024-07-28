4ddc4cc570f1dff26da87cd4f173271a774a7603
Arian <arian.kulmer@web.de>
2024-07-28 17:56:22 +0200

    vermeer: Enable light sensor notifier
    Change-Id: Iddde3d482190e183f9e1dfbb8d7a1327abd697f2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,3 +25,4 @@ TARGET_OTA_ASSERT_DEVICE := vermeer
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/configs/properties/odm.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/properties/vendor.prop
