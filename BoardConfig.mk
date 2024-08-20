b71f417c36c7c2dcee731c3d6eb363af3b8aa9e3
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-08-20 12:11:11 +0200

    vermeer: Switch to inline kernel_platform kernel building
    Change-Id: I574add9e78b2f792aec6e6deae735408ebe5fce1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,17 +12,6 @@ include device/xiaomi/sm8550-common/BoardConfigCommon.mk
 # Display
 TARGET_SCREEN_DENSITY := 530
 
-# Kernel Modules
-device_second_stage_modules := \
-    wl2866d.ko \
-    wl2868c.ko \
-    xiaomi_touch.ko \
-    goodix_ts.ko
-
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES += $(addprefix $(KERNEL_PREBUILT_DIR)/vendor_dlkm/, $(device_second_stage_modules))
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD += $(device_second_stage_modules)
-BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += $(device_second_stage_modules)
-
 # OTA
 TARGET_OTA_ASSERT_DEVICE := vermeer
 
--- device.mk
+++ device.mk
@@ -14,9 +14,6 @@ $(call inherit-product, vendor/xiaomi/vermeer/vermeer-vendor.mk)
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
-# Kernel Binary
-KERNEL_PREBUILT_DIR := device/xiaomi/vermeer-kernel
-
 # Overlays
 PRODUCT_PACKAGES += \
     VermeerFrameworks \
